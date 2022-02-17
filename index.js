aa920def534e1eecf44dcec7937062f55e03ba04
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2022-02-17 14:57:46 +0000

    add vapi

--- index.js
+++ index.js
@@ -1,122 +1,13 @@
-import { neru, Voice, Scheduler, Messages } from 'neru-alpha';
-
+import { Messages, Scheduler, Voice, neru } from 'neru-alpha';
 const router = neru.Router();
+
 const session = neru.createSession();
-const voice =  new Voice(session);
+const voice = new Voice(session);
+const messaging = new Messages(session);
 
 const contact = JSON.parse(process.env['NERU_CONFIGURATIONS']).contact;
-await voice.onInboundCall('onCall', contact).execute();
 
-router.post('/onCall', async (req, res, next) => {
-    try {
-        const session = neru.createSession();
-        const state = session.getState();
-        const voiceApi = new Voice(session);
-
-        const conversation = await voiceApi.createConversation();
-
-        await conversation.acceptInboundCall(req.body).execute();
-        await state.set('conversationData', {
-            id: conversation.id,
-            name: conversation.name,
-            number: req.body.body.channel.from.number
-        });
-        await state.set('flowState', 'id');
-        await conversation.onDTMF('onDtmf').execute();
-        await conversation.sayText({ 
-            text: `Welcome to VonagePark,
-            enter the car park ID followed by star to continue.`
-        }).execute();
-
-        res.status(200);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.post('/onDtmf', async (req, res, next) => {
-    try {
-        const session = neru.getSessionFromRequest(req)
-        const state = session.getState();
-        const voice = new Voice(session);
-
-        const digit = req.body.body.digit;
-        
-        const flowState = await state.get("flowState");
-        const conversationData = await state.get("conversationData");
-        const conversation = voice.getConversation(conversationData.id, conversationData.name);
-
-        switch (flowState) {
-            case 'id':
-                if (digit === '*') {
-                    const parkingID = await state.get("dtmfDigits");
-                    await state.set('flowState', 'hour');
-                    await conversation.sayText({ text: `You are parking at ${parkingID}.
-                    Press a digit to choose how many hours you want to pay for.`}).execute();
-                } else {
-                    const currentDigits = await state.get("dtmfDigits");
-                    if (currentDigits != null) {
-                        await state.set("dtmfDigits", currentDigits+digit);
-                    } else {
-                        await state.set("dtmfDigits", digit);
-                    }
-                }
-                break;
-            case 'hour':
-                await state.set("hours", digit);
-                await state.set('flowState', 'pay');
-                await conversation.sayText({text: `You are parking for ${digit} hours. 
-                Enter your card number followed by a star to pay.`}).execute();
-                break;
-            case 'pay':
-                const messaging = new Messages(session);
-                const scheduler = new Scheduler(session);
-                if (digit === '*') {
-                    const hours = await state.get("hours");
-                    const parkingID = await state.get("dtmfDigits");
-                    
-                    await chargeCard();
-                    await conversation.sayText({text: `Your card has been charged for ${hours} hours. 
-                    You will receive a text confirmation and a reminder when your parking is about to expire`}).execute();
-        
-                    const to = { type: "sms", number: conversationData.number };
-                    const from = { type: "sms", number: contact.number }; 
-        
-                    await messaging.sendText(
-                        from,
-                        to,
-                        `You are parking at ${parkingID} and have paid for ${hours} hours.`
-                    ).execute();
-        
-                    const endTime = new Date(new Date().setHours(new Date().getHours() + parseInt(hours)));
-                    const testTime = new Date(new Date().setSeconds(new Date().getSeconds() + 10));
-        
-                    scheduler.startAt({
-                        // startAt: new Date(endTime.getTime() - (1000 * 5)).toISOString(),
-                        startAt: testTime.toISOString(),
-                        callback: 'parkingReminder',
-                        payload: {
-                            text: 'hello world!',
-                        }
-                    }).execute();
-
-                    await state.set('flowState', '');
-
-                } else {
-                    const currentDigits = await state.get("cardDigits");
-                    if (currentDigits != null) {
-                        await state.set("cardDigits", currentDigits+digit);
-                    } else {
-                        await state.set("cardDigits", digit);
-                    }
-                }
-                break;
-        }
-        res.status(200);
-    } catch (error) {
-        next(error);
-    }
-});
+await voice.onVapiAnswer('onCall').execute();
 
 async function chargeCard() {
 	return new Promise((resolve) => {
@@ -124,22 +15,194 @@ async function chargeCard() {
       });
 }
 
+router.post('/onCall', async (req, res, next) => {
+    try {
+        const session = neru.createSession();
+        const voice = new Voice(session);
+        const messaging = new Messages(session);
+        const state = session.getState();
+
+        await voice.onVapiEvent(req.body.uuid, 'onEvent').execute();
+        await messaging.listenMessages(
+            { type: "sms", number: req.body.from }, 
+            { type: "sms", number: contact.number },
+            'onMessage'
+        ).execute(); 
+        await state.set("calldata", {
+            flowState: 'hour'
+        });
+
+        res.json([
+            {
+                action: 'talk',
+                text: 'Welcome to VonagePark, enter the car park ID, followed by a hash to continue',
+                bargeIn: true
+            },
+            {
+                action: 'input',
+                type: ['dtmf'],
+                dtmf: {
+                    timeOut: '10',
+                    submitOnHash: true
+                }
+            }
+        ]);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/onEvent', async (req, res, next) => {
+    try {
+        if (req.body.dtmf != null) {
+            const session = neru.getSessionFromRequest(req);
+            const state = session.getState();
+            const messaging = new Messages(session);
+            
+            const from = req.body.from;
+            const to = { type: "sms", number: from };
+            const vonageNumber = { type: "sms", number: contact.number }; 
+
+            const digits = req.body.dtmf.digits;
+            const data = await state.get("calldata");
+
+            switch (data.flowState) {
+                case 'hour':
+                    await state.set("calldata", {
+                        flowState: 'reg',
+                        parkingID: digits
+                    });
+
+                    res.json([
+                        {
+                            action: 'talk',
+                            text: `You are parking at ${digits}. Press a digit to choose how many hours you want to pay for.`,
+                            bargeIn: true
+                        },
+                        {
+                            action: 'input',
+                            type: ['dtmf'],
+                            dtmf: {
+                                timeOut: '10',
+                                submitOnHash: true
+                            }
+                        }
+                    ]);
+                    break;
+
+                case 'reg':            
+                    await messaging.sendText(
+                        vonageNumber,
+                        to,
+                        `Please reply with your car's registration number`
+                    ).execute();
+
+                    res.json([
+                        {
+                            action: 'talk',
+                            text: `You will receive a text from this number, reply with your car's registration number. then press any digit.`
+                        },
+                        {
+                            action: 'stream',
+                            streamUrl: ["https://onhold2go.co.uk/song-demos/free/a-new-life-preview.mp3"],
+                            loop: "0"
+                        }
+                    ]);
+
+                    break;
+
+                case 'pay':
+                    res.json([
+                        {
+                            action: 'talk',
+                            text: `You are parking for ${digits} hours. Enter your card number followed by a hash to pay.`,
+                            bargeIn: true
+                        },
+                        {
+                            action: 'input',
+                            type: ['dtmf'],
+                            dtmf: {
+                                timeOut: '10',
+                                submitOnHash: true
+                            }
+                        }
+                    ]);
+                    break;
+
+                case 'complete':
+                    const scheduler = new Scheduler(session);
+
+                    await chargeCard();
+            
+                    await messaging.sendText(
+                        vonageNumber,
+                        to,
+                        `You are parking at ${data.parkingID} and have paid for ${data.hours} hours.`
+                    ).execute();
+
+                    const endTime = new Date(new Date().setHours(new Date().getHours() + parseInt(data.hours)));
+                    const testTime = new Date(new Date().setSeconds(new Date().getSeconds() + 20));
+
+                    scheduler.startAt({
+                        // startAt: new Date(endTime.getTime() - (1000 * 5)).toISOString(),
+                        startAt: testTime.toISOString(),
+                        callback: 'parkingReminder',
+                        payload: {
+                            from: from,
+                        }
+                    }).execute();
+
+                    res.json([
+                        {
+                            action: 'talk',
+                            text: `Your card has been charged for ${data.hours} hours. You will receive a text confirmation and a reminder when your parking is about to expire`,
+                        }
+                    ]);
+                    break;
+            }
+
+        } else {
+            console.log(req.body)
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/onMessage', async (req, res, next) => {
+    try {
+        const session = neru.getSessionFromRequest(req)
+        const state = session.getState();
+        const data = await state.get("calldata");
+        await state.set("calldata", {
+            flowState: 'pay',
+            parkingID: data.parkingID,
+            hours: data.hours,
+            reg: req.body.message.content.text
+        });
+
+        res.status(200);
+    } catch (error) {
+        next(error);
+    }
+});
+
 router.post('/parkingReminder', async (req, res, next) => {
     try {
         const session = neru.getSessionFromRequest(req)
         const state = session.getState();
         const messaging = new Messages(session);
 
-        const parkingID = await state.get("dtmfDigits");
-        const conversationData = await state.get("conversationData");
+        const from = req.body.from;
+        const data = await state.get("calldata");
 
-        const to = { type: "sms", number: conversationData.number };
-        const from = { type: "sms", number: contact.number }; 
+        const to = { type: "sms", number: from };
+        const vonageNumber = { type: "sms", number: contact.number }; 
 
         await messaging.sendText(
-            from,
+            vonageNumber,
             to,
-            `Your parking at ${parkingID} is about to run out.`
+            `Your parking at ${data.parkingID} is about to run out.`
         ).execute();
 
         res.status(200);
